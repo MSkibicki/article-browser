65cd119ef4dd7a78964686a13350eb753fb117a7
MSkibicki <m.skibicki777@gmail.com>
2020-05-04 20:26:27 +0200

    add single-article-page component styles

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import "./SingleArticle.scss";
 
 const SingleArticle = ({ articles, id }) => {
   const [pageArticles, setPageArticles] = useState({});
@@ -9,16 +10,24 @@ const SingleArticle = ({ articles, id }) => {
     setPageArticles(articles[id]);
   }, [articles, id]);
 
-  //   console.log(id);
-
   return (
-    <div>
-      <h1>{pageArticles.title}</h1>
-      <p>{pageArticles.author}</p>
-      <p>{pageArticles.publishedAt}</p>
-      <img src={pageArticles.urlToImage} alt="article-visual" />
-      <p>{pageArticles.content}</p>
-      <Link to="/">back</Link>
+    <div className="single-article">
+      <h1 className="single-article-title">{pageArticles.title}</h1>
+      <div className="single-article-author-group">
+        <h3 className="single-article-author">{pageArticles.author}</h3>
+        <p className="single-article-date">{pageArticles.publishedAt}</p>
+      </div>
+      <div className="single-article-img-group">
+        <img
+          className="single-article-img"
+          src={pageArticles.urlToImage}
+          alt="article-visual"
+        />
+        <p className="single-article-paragraph">{pageArticles.content}</p>
+      </div>
+      <Link className="single-article-button" to="/">
+        Back to Homepage
+      </Link>
     </div>
   );
 };
